f584d0cf44841983d50e6efafc85e99e9bb3ce39
stamen.peev <stamen.peev@softwaregroup-bg.com>
2018-05-30 13:51:46 +0300

    feat: enable metrics and optimize code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const swagger2Koa = require('swagger2-koa');
 const swaggerParser = require('swagger-parser');
 const errorsFactory = require('./errorsFactory');
+const utils = require('./utils');
 module.exports = (params = {}) => {
     const Port = params.parent;
     class SwaggerPort extends Port {
@@ -18,7 +19,7 @@ module.exports = (params = {}) => {
                 skipPaths: []
                 // http server connection options
                 // https://nodejs.org/api/net.html#net_server_listen_options_callback
-                // port, host, path, backlog, exclusive
+                // port, host, path, backlog, exclusive, readableAll, writableAll
             }, params.config);
             Object.assign(this.errors, errorsFactory(this.bus));
         }
@@ -35,34 +36,46 @@ module.exports = (params = {}) => {
             await swaggerParser.validate(swaggerDocument);
             const swaggerRouter = swagger2Koa.router(swaggerDocument);
             const paths = Object.keys(swaggerDocument.paths);
-            if (this.log.trace) {
-                this.log.trace({$meta: {mtid: 'config', opcode: 'paths'}, message: paths.sort()});
-            }
             paths.forEach(path => {
                 Object.keys(swaggerDocument.paths[path]).forEach(method => {
-                    const busMethod = this.bus.importMethod(swaggerDocument.paths[path][method]['x-method']);
+                    const methodName = swaggerDocument.paths[path][method]['x-bus-method'];
                     swaggerRouter[method](path, async (ctx, next) => {
                         const {params, query} = ctx;
                         const {body} = ctx.request;
+                        const trace = utils.uuid.v4();
                         if (this.log.trace) {
-                            this.log.trace({$meta: {mtid: 'request'}, body, params, query});
+                            this.log.trace({$meta: {mtid: 'request', trace}, body, params, query});
                         }
-                        try {
-                            ctx.body = await busMethod(Object.assign({}, body, params, query));
-                            ctx.status = 200;
-                            if (this.log.trace) {
-                                this.log.trace({$meta: {mtid: 'response'}, body: ctx.body, status: ctx.status});
-                            }
-                        } catch (error) {
-                            ctx.status = (error.details && error.details.statusCode) || 400;
-                            ctx.body = {
-                                error
+                        return new Promise(resolve => {
+                            const msg = Object.assign({}, body, params, query);
+                            const $meta = {
+                                trace,
+                                mtid: 'request',
+                                method: methodName,
+                                reply: (response, $responseMeta) => {
+                                    switch ($responseMeta.mtid) {
+                                        case 'response':
+                                            ctx.body = response;
+                                            ctx.status = 200;
+                                            break;
+                                        case 'error':
+                                            ctx.status = (response.details && response.details.statusCode) || 400;
+                                            ctx.body = {
+                                                error: response
+                                            };
+                                            break;
+                                        default:
+                                            ctx.status = 400;
+                                            ctx.body = {
+                                                error: this.errors.swagger({cause: response})
+                                            };
+                                            break;
+                                    }
+                                    return resolve(next());
+                                }
                             };
-                            if (this.log.error) {
-                                this.log.error({$meta: {mtid: 'error'}, body: ctx.body, status: ctx.status});
-                            }
-                        }
-                        await next();
+                            this.stream.push([msg, $meta]);
+                        });
                     });
                 });
             });
@@ -76,16 +89,18 @@ module.exports = (params = {}) => {
                     }
                 }
             });
-            let {port, host, path, backlog, exclusive} = this.config;
+            let {port, host, path, backlog, exclusive, readableAll, writableAll} = this.config;
             this.server = app
                 .use(swagger2Koa.ui(swaggerDocument, this.config.pathRoot, this.config.skipPaths))
-                .listen({port, host, path, backlog, exclusive});
+                .listen({port, host, path, backlog, exclusive, readableAll, writableAll});
+
             this.log.info && this.log.info({
                 message: 'Swagger port started',
                 address: this.server.address(),
                 $meta: {
                     mtid: 'event',
-                    opcode: 'port.started'
+                    opcode: 'port.started',
+                    paths: paths.sort()
                 }
             });
         }
