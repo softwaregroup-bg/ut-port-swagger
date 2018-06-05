6548f3ff3be712e8c74cfaa64bd77b74673aa419
stamen.peev <stamen.peev@softwaregroup-bg.com>
2018-06-05 17:03:20 +0300

    fix: optimize code

--- middleware/swaggerUI/index.js
+++ middleware/swaggerUI/index.js
@@ -1,26 +1,27 @@
 const send = require('koa-send');
-const SWAGGER_UI_PATH = require('swagger-ui-dist').getAbsoluteFSPath();
+const root = require('swagger-ui-dist').getAbsoluteFSPath();
 const html = require('./html');
 module.exports = (port, swaggerDocument) => {
     const {pathRoot, skipPaths} = port.config;
     const pathPrefix = pathRoot.endsWith('/') ? pathRoot : pathRoot + '/';
     const htmlBody = html(swaggerDocument, pathPrefix);
     return async (context, next) => {
-        if (context.path.startsWith(pathRoot)) {
-            const skipPath = skipPaths.some((current) => context.path.startsWith(current));
-            if (context.path === pathRoot && context.method === 'GET') {
+        if (context.method === 'GET' && context.path.startsWith(pathRoot)) {
+            if (context.path === pathRoot) {
                 context.type = 'text/html; charset=utf-8';
                 context.body = htmlBody;
                 context.status = 200;
                 return;
-            } else if (context.path === (pathPrefix + 'api-docs') && context.method === 'GET') {
+            }
+            if (context.path === (pathPrefix + 'api-docs')) {
                 context.type = 'application/json; charset=utf-8';
                 context.body = swaggerDocument;
                 context.status = 200;
                 return;
-            } else if (!skipPath && context.method === 'GET') {
+            }
+            if (!skipPaths.some(current => context.path.startsWith(current))) {
                 const filePath = context.path.substring(pathRoot.length);
-                await send(context, filePath, { root: SWAGGER_UI_PATH });
+                await send(context, filePath, { root });
                 return;
             }
         }
