285b0d2a6e3f72841624b8d56ed81a56b80cc499
stambata <stamen.peev@softwaregroup-bg.com>
2018-06-09 10:15:32 +0300

    fix: remove lodash.merge dependency

--- middleware/validator/2/index.js
+++ middleware/validator/2/index.js
@@ -1,7 +1,6 @@
 const swagger2 = require('swagger2');
-const merge = require('lodash.merge');
 module.exports = ({port, swaggerDocument}) => {
-    let document = merge({}, swaggerDocument);
+    let document = port.merge({}, swaggerDocument);
     let formFiles = {};
     Object.keys(document.paths).forEach(path => {
         Object.keys(document.paths[path]).forEach(method => {
