f6b4c629abc2122b42c05f8847241180e1812678
stamen.peev <stamen.peev@softwaregroup-bg.com>
2018-06-21 14:47:59 +0300

    fix: add custom keyword x-occurances

--- middleware/validator/custom/compile/ajv.js
+++ middleware/validator/custom/compile/ajv.js
@@ -13,6 +13,50 @@ class UtAjv extends Ajv {
                 return schema === true ? typeof value !== 'undefined' : false;
             }
         });
+        this.addKeyword('x-occurrences', {
+            type: 'array',
+            compile: schema => value => {
+                return schema
+                    .map(rule => {
+                        const count = (value || [])
+                            .map(record => {
+                                return record && record[rule.key] === rule.value;
+                            })
+                            .filter(x => x)
+                            .length;
+                        return rule.min <= count && count <= rule.max;
+                    })
+                    .filter(x => x)
+                    .length === schema.length;
+            },
+            metaSchema: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    additionalProperties: false,
+                    required: ['key', 'value', 'min', 'max'],
+                    properties: {
+                        key: {
+                            description: 'key',
+                            type: 'string'
+                        },
+                        value: {
+                            description: 'value'
+                        },
+                        min: {
+                            description: 'min',
+                            type: 'integer',
+                            min: 0
+                        },
+                        max: {
+                            description: 'min',
+                            type: 'integer',
+                            min: 0
+                        }
+                    }
+                }
+            }
+        });
     }
 }
 
