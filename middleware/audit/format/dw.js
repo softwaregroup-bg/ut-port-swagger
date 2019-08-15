45580ed2ce7797780cc0eb79ac84f01bdfe67200
stamen.peev <stamen.peev@softwaregroup-bg.com>
2019-08-15 11:52:24 +0300

    fix: refactor dw format

--- middleware/audit/format/dw.js
+++ middleware/audit/format/dw.js
@@ -1,36 +1,38 @@
 const os = require('os');
-const serverMachineName = os.hostname();
-const serverOsVersion = [os.type(), os.platform(), os.release()].join(':');
+const destinationMachineName = os.hostname();
+const serverOs = [os.type(), os.platform(), os.release()].join(':');
+const uuid = require('uuid').v4;
 module.exports = (port, ctx, error) => {
     const {
         sessionId = null,
         businessUnitId = null,
         businessUnitName = null,
         userId = null,
-        username: userName = null
+        username: userName = null,
+        tenantId: tenantUUID = null,
+        tenantName = null
     } = ctx.ut.$meta.auth;
 
     return {
         auditEntryId: null,
-        dateAndTime: null,
         success: !error,
-        failureReason: error ? error.message : null,
-        failureCode: error ? (error.type || error.code || error.name) : null,
-        relatedObjects: [
+        errorMessage: error ? error.message : null,
+        errorCode: error ? (error.type || error.code || error.name) : null,
+        affectedObjects: [
             {
                 objectId: null,
                 objectType: null
             }
         ],
         callParams: ctx.ut.msg,
-        eventGUID: ctx.ut.$meta.trace,
-        eventGUIDDateTime: Date.now() / 1000 | 0, // unix timestamp
-        eventClass: port.config.id,
-        eventCode: ctx.ut.method,
+        traceUUID: ctx.ut.$meta.trace,
+        eventUUID: uuid(),
+        eventKey: ctx.ut.$meta.method,
         eventURI: ctx.url,
+        eventDate: Date.now() / 1000 | 0, // unix timestamp
         eventDescription: ctx.ut.method,
-        controllerName: ctx.ut.method.split('.')[0],
-        controllerVersion: port.bus.config.version,
+        serviceName: ctx.ut.method.split('.')[0], // to do
+        serviceVersion: port.bus.config.version, // to do
         channel: 'web',
         userId,
         userName,
@@ -38,15 +40,19 @@ module.exports = (port, ctx, error) => {
         businessUnitId,
         severityLevel: null,
         sessionId,
-        sourceIpAddress: ctx.req.socket.remoteAddress,
-        destinationIpAddress: ctx.req.socket.localAddress,
+        sourceIp: ctx.req.socket.remoteAddress,
+        destinationIp: ctx.req.socket.localAddress,
         destinationPort: ctx.req.socket.localPort,
         geolocation: null,
-        serverOsVersion,
-        domainName: ctx.headers['x-forwarded-host']
+        serverOs,
+        destinationHostName: ctx.headers['x-forwarded-host']
             ? ctx.headers['x-forwarded-host'].split(',')[0]
             : ctx.hostname,
-        serverMachineName,
-        deviceId: null
+        destinationMachineName,
+        userDeviceId: null,
+        tenantUUID,
+        tenantName,
+        eventMappingPath: null,
+        eventMappingVia: null
     };
 };
